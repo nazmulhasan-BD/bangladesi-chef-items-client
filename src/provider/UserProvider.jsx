c9822741b4e64b1b4f1b61ed6c4544161e380bea
nazmulhasan-BD <munnanazmu72@gmail.com>
2024-02-24 22:31:05 +0600

    firebase file recreate and past file hide from history

--- src/provider/UserProvider.jsx
+++ src/provider/UserProvider.jsx
@@ -1,6 +1,6 @@
 import { createContext, useEffect, useState } from 'react';
 import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signOut, updateProfile } from "firebase/auth";
-import app from '../firebase/firebase.config';
+import app from '../firebase-config/firebase.config';
 
 export const UserContext = createContext(null)
 
